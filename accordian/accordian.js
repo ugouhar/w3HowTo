3d9d0b276e26befbf798c19aa7c5a1700de689c0
gouhar1512 <ushran987@gmail.com>
2023-08-14 23:44:33 +0530

    Added Accordian

---
+++ accordian/accordian.js
@@ -0,0 +1,15 @@
+let sections = document.querySelectorAll(".section");
+
+sections.forEach((section) => {
+  section.addEventListener("click", () => {
+    let heading = section.querySelector(".heading");
+    heading.classList.toggle("expanded");
+
+    let expandButton = heading.querySelectorAll("div")[1];
+    let text = expandButton.innerText;
+    expandButton.innerText = text == "+" ? "-" : "+";
+
+    let content = section.querySelector(".content");
+    content.classList.toggle("hidden");
+  });
+});
