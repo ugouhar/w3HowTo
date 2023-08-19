bde8c7c2704ac5ed1189b4579cf603971bf83371
gouhar1512 <ushran987@gmail.com>
2023-08-19 23:55:14 +0530

    Added Horizontal Tabs

---
+++ tabs/tabs.js
@@ -0,0 +1,15 @@
+let tabs = document.querySelectorAll(".tab");
+function switchTab(index) {
+  let horizontalTabs = document.querySelector(".horizontal-tabs");
+
+  tabs.forEach((tab) => tab.classList.remove("active"));
+  tabs[index].classList.add("active");
+
+  let contents = horizontalTabs.querySelectorAll(".content");
+  contents.forEach((content) => content.classList.add("hidden"));
+  contents[index].classList.remove("hidden");
+}
+
+tabs.forEach((tab, index) =>
+  tab.addEventListener("click", () => switchTab(index))
+);
