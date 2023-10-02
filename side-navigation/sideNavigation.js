9f468e40a561c1dfe3b0ea65f71863dc5b906037
gouhar1512 <ushran987@gmail.com>
2023-10-02 13:32:18 +0530

    Side nav more behaviour

--- side-navigation/sideNavigation.js
+++ side-navigation/sideNavigation.js
@@ -1,10 +1,19 @@
 const menuIcon = document.querySelector(".menu-icon");
 const sideNav = document.querySelector(".sidenav");
 const closeBtn = document.querySelector(".close-btn");
+const sideNavPush = document.querySelector(".sidenav-push");
+const sideNavFullWidth = document.querySelector(".sidenav-full-width");
+const container = document.querySelector(".container");
 
 const toggleSideNav = () => {
-  sideNav.classList.toggle("show");
+  // sideNav.classList.toggle("show");
+  // sideNavPush.classList.toggle("show2");
+  // container.classList.toggle("container-push");
+  sideNavFullWidth.classList.toggle("sidenav-full-width-expanded");
 };
 
 menuIcon.addEventListener("click", toggleSideNav);
 closeBtn.addEventListener("click", toggleSideNav);
+document
+  .querySelectorAll(".close-btn2")[1]
+  .addEventListener("click", toggleSideNav);
